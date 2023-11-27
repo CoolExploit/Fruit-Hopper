8bafd6ea43d3010f942b2f544b046151616ce9ca
CoolExploit <117057140+CoolExploit@users.noreply.github.com>
2023-11-27 20:58:27 +0800

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,27 @@
+--brought to you by scriptsomg.com
+
+Fruits = {
+"Quake Fruit",
+"Blizzard Fruit",
+"Portal Fruit",
+"String Fruit",
+"Human: Buddha Fruit",
+"Bird: Phoenix Fruit",
+"Rumble Fruit",
+"Paw Fruit",
+"Gravity Fruit",
+"Dough Fruit",
+"Control Fruit",
+"Venom Fruit",
+"Shadow Fruit",
+"Dragon Fruit" ,
+"Soul Fruit",
+"Leopard Fruit"
+}
+Valuable = {"Quake Fruit","String Fruit","Portal Fruit","Blizzard Fruit","Dough Fruit","Shadow Fruit","Venom Fruit","Control Fruit","Dragon Fruit","Soul Fruit","Leopard Fruit"} ------ it will stop the script if you got this fruit
+Webhook = "https://discordapp.com/api/webhooks/1177937465558970380/wKtrKRbUQE5r4ezzt3HZTfxIzI1xqQdPDJWrdNebNGn2Cjv7nbZaWt1Fe-DoY3esGULZ" --------------------------------Optional
+Store = true  --------------------------------Auto Store after it got the fruit
+Safeplace = false ----------------------------- Safeplace 
+Repeat = true --------------------------------- Repeat continues hopping
+-----------------------------------------------------------------------
+loadstring(game:HttpGet"https://gist.githubusercontent.com/NotHubris/4e6fdc88d84c30afa9b28c590f273bbf/raw")()
